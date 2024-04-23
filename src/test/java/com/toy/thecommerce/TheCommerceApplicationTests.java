19109bb1320cc5a850f6776b42c29581a27260d6
nadoran78 <nadoran78@gmail.com>
2024-04-23 10:00:26 +0900

    project init

---
+++ src/test/java/com/toy/thecommerce/TheCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy.thecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheCommerceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
