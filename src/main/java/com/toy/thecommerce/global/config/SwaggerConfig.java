1d70da0e1dc691ac26b594be03455723c6ce1a24
nadoran78 <nadoran78@gmail.com>
2024-04-24 10:37:31 +0900

    feat: swagger 설정

---
+++ src/main/java/com/toy/thecommerce/global/config/SwaggerConfig.java
@@ -0,0 +1,34 @@
+package com.toy.thecommerce.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.basePackage("com.toy.thecommerce"))
+        .paths(PathSelectors.any())
+        .build()
+        .apiInfo(apiInfo());
+  }
+
+  private ApiInfo apiInfo() {
+    return new ApiInfoBuilder()
+        .title("The Commerce Toy Project")
+        .description("더 커머스 채용 관련 toy project")
+        .version("0.1")
+        .build();
+  }
+}
