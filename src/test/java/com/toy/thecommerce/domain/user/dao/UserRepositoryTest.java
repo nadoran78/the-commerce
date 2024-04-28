51836f6bdfdd42599af97c8a10f5349390ca3137
nadoran78 <nadoran78@gmail.com>
2024-04-28 18:25:59 +0900

    test: jpa 정렬 기준 test 수정

--- src/test/java/com/toy/thecommerce/domain/user/dao/UserRepositoryTest.java
+++ src/test/java/com/toy/thecommerce/domain/user/dao/UserRepositoryTest.java
@@ -26,7 +26,7 @@ class UserRepositoryTest {
   private TestEntityManager testEntityManager;
 
   @BeforeEach
-  void saveTestUser() {
+  void saveTestUser() throws InterruptedException {
     for (int i = 0; i < 10; i++) {
       User user = User.builder()
           .userId("userId" + i)
@@ -37,13 +37,14 @@ class UserRepositoryTest {
           .email("email" + i)
           .build();
       testEntityManager.persist(user);
+      Thread.sleep(1);
     }
   }
 
   @Test
   void successFindAll_whenPageRequestByCreatedAt() {
     //given
-    PageRequest pageRequest = PageRequest.of(0, 10, Direction.DESC, "createdAt");
+    PageRequest pageRequest = PageRequest.of(0, 5, Direction.DESC, "createdAt");
     //when
     Page<User> pageResult = userRepository.findAll(pageRequest);
 
@@ -54,35 +55,26 @@ class UserRepositoryTest {
     assertEquals(9, resultList.get(1).getId());
     assertEquals(8, resultList.get(2).getId());
     assertEquals(7, resultList.get(3).getId());
-    assertEquals(6, resultList.get(4).getId());
-    assertEquals(5, resultList.get(5).getId());
-    assertEquals(4, resultList.get(6).getId());
-    assertEquals(3, resultList.get(7).getId());
-    assertEquals(2, resultList.get(8).getId());
-    assertEquals(1, resultList.get(9).getId());
+    assertEquals(7, resultList.get(4).getId());
+
 
   }
 
   @Test
   void successFindAll_whenPageRequestByUsername() {
     //given
-    PageRequest pageRequest = PageRequest.of(0, 10, Direction.ASC, "username");
+    PageRequest pageRequest = PageRequest.of(1, 5, Direction.ASC, "username");
     //when
     Page<User> pageResult = userRepository.findAll(pageRequest);
 
     //then
     List<User> resultList = pageResult.getContent();
 
-    assertEquals("a", resultList.get(0).getUsername());
-    assertEquals("b", resultList.get(1).getUsername());
-    assertEquals("c", resultList.get(2).getUsername());
-    assertEquals("d", resultList.get(3).getUsername());
-    assertEquals("e", resultList.get(4).getUsername());
-    assertEquals("f", resultList.get(5).getUsername());
-    assertEquals("g", resultList.get(6).getUsername());
-    assertEquals("h", resultList.get(7).getUsername());
-    assertEquals("i", resultList.get(8).getUsername());
-    assertEquals("j", resultList.get(9).getUsername());
+    assertEquals("f", resultList.get(0).getUsername());
+    assertEquals("g", resultList.get(1).getUsername());
+    assertEquals("h", resultList.get(2).getUsername());
+    assertEquals("i", resultList.get(3).getUsername());
+    assertEquals("j", resultList.get(4).getUsername());
 
   }
 
